14414660b535b7930372d072775e7a1872af0cab
xal_Service <240720200@qq.com>
2019-09-27 21:19:18 +0800

    first helloworld

---
+++ src/main/java/com/example/helloworld/controller/UserController.java
@@ -0,0 +1,15 @@
+package com.example.helloworld.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @GetMapping("/login")
+    public String login(String name){
+        return "user:"+name;
+    }
+}
