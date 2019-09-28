0f1791afe2b4a04c7d7da46852a8681f43a757b8
xal_Service <240720200@qq.com>
2019-09-28 16:23:27 +0800

    修改启动类，不然打成war后不能再tomcat运行

--- src/main/java/com/example/helloworld/HelloworldApplication.java
+++ src/main/java/com/example/helloworld/HelloworldApplication.java
@@ -2,12 +2,21 @@ package com.example.helloworld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class HelloworldApplication {
-
+public class HelloworldApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(HelloworldApplication.class, args);
     }
-
+    /**
+     * springboot项目打成war包部署到tomcat时需要改变启动方式，否则运行tomcat时war包只会解压，但是加载不了
+     * SpringBoot启动类继承SpringBootServletInitializer方法，并重写configure方法
+     */
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        // 注意这里要指向原先用main方法执行的Application启动类
+        return builder.sources(HelloworldApplication.class);
+    }
 }
